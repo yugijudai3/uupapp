55392e5aff21d1c6f5152f5ef1e9a52b92d6b4f8
EC2 Default User <ec2-user@ip-172-31-43-213.us-east-2.compute.internal>
2019-05-11 02:23:22 +0000

    aaa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,67 @@
 import { Component } from '@angular/core';
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  post:{
+    username: string,
+    message: string,
+    createdDate: any
+  };
+  message: string;
+  
+  constructor(private alertCtrl: AlertController){}
+  
+  posts:{username: string, message: string, createddate: any}[]
+  =[{
+    username: "大根",
+    message: "お前を殺す",
+    createddate: "10分前"
+  },{
+    username: "MSZ",
+    message: "お前を殺す",
+    createddate: "5分前"
+  }];
+  
+  addPost(){
+    this.post={
+      username: "遊戯",
+      message: this.message,
+      createddate: "数秒前"
+    };
+    this.posts.push(this.post);
+    this.message="";
+  }
+  
+  async presentPrompt(index: number){
+    const alert = await this.alertCtrl.create({
+      header: "メッセージ編集",
+      inputs:[{
+        name: "message",
+        type: "text",
+        placeholder: "メッセージ"
+      }],
+      buttons:[{
+        text: "キャンセル",
+        role: "cancel",
+        handler: () => {console.log("キャンセルが選択されました");}
+      },
+      {
+        text: "更新",
+        handler: data => {
+          console.log(data);
+          this.posts[index].message = data.message;
+        }
+      }]
+    });
+    await alert.present();
+  }
+  
+  deletePost(index: number){
+    this.posts.splice(index, 1);
+  }
+}
